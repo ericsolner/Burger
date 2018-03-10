73713e0060b61d66e5bf8b67a3072a27e83262eb
Eric Solner <ericsolner2018@u.northwestern.edu>
2018-03-10 11:44:25 -0600

    commit3

--- server.js
+++ server.js
@@ -21,4 +21,7 @@ app.use('/', router);
 
 var port = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
